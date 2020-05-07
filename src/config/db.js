bc08c63996e434b498d857feea2ef1c0e7030297
DreamsOfImran <ibasha66@gmail.com>
2020-05-08 00:18:40 +0530

    Adding db name for production

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  dbName: "library",
+  dbName: process.env.DB_NAME,
 });
 
 const db = mongoose.connection;
